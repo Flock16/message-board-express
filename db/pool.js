73693db864247ac5815a5bec5434a0a6a4d296f6
Ben <ben.flockhart@outlook.com>
2025-02-14 21:28:06 +1000

    remove hard coded db url

--- db/pool.js
+++ db/pool.js
@@ -3,8 +3,7 @@ const { Pool } = pkg;
 import "dotenv/config";
 
 const pool = new Pool({
-  // connectionString: `${process.env.DATABASE_URL}`,
-  connectionString: `postgresql://postgres:FUShcvjnUlOzPxexrRySrUrPqnKzxfyB@viaduct.proxy.rlwy.net:48971/railway`,
+  connectionString: `${process.env.DATABASE_URL}`,
 });
 
 export default pool;
